678e278985189aa967de33bd9d01447545e574d6
Abdussamed DUZ <68667069+shaqablanca@users.noreply.github.com>
2023-07-13 00:51:43 +0200

    Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Dictionary
+{
+    using System;
+    using System.Reflection;
+
+    public class MyClass
+    {
+        public int MyProperty { get; set; }
+        public void MyMethod()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Type type = typeof(MyClass);
+            PropertyInfo property = type.GetProperty("MyProperty");
+            MethodInfo method = type.GetMethod("MyMethod");
+            
+            Console.WriteLine("Property: " + property.ToString());
+            Console.WriteLine("Method: " + method.Name);
+        }
+    }
+
+}
