07ba62451b09d2c0e7757828b8ea2d56cf6c4adc
Abdussamed DUZ <68667069+shaqablanca@users.noreply.github.com>
2023-07-13 00:49:20 +0200

    Practice

---
+++ BuildArrayFromPermutation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BuildArrayFromPermutation
+{
+   
+    public class BuildArrayFromPermutation
+    {
+        static void Main(String[] args)
+        {
+            int[] nums = {0,2,1,5,3,4};
+            int[] ans = Array.Empty<int>();
+            int i = 0;
+
+            while(i<nums.Length)
+            {
+                ans = ans.Append(nums[nums[i]]).ToArray();
+                i++;
+            }
+
+            Console.WriteLine(string.Join("-", ans));
+           
+        }
+              
+    }
+}
---
+++ JumpGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Algorithm
+{
+   public  class PowerOfNumber
+    {
+
+        public int Power(int power,int number)
+        {
+            int result = 1;
+            if (power > 0)
+            {
+                for (int i = 0; i < power; i++)
+                {
+                    result *= number;
+                }
+            }
+            else if (power < 0)
+            {
+                number = 1 / number;
+                result *= number;
+            }
+            else
+            {
+                result = 1;
+            }
+            return result;
+
+        }
+
+        public static void Main(String[] args)
+        {
+            PowerOfNumber sonuc = new PowerOfNumber();
+
+            Console.WriteLine(sonuc.Power(2, 15));
+        }
+    }
+
+}
