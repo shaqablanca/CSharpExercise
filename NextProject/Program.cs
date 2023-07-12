56ca87b42c0fabdd6d1b2a18c276351f1b542c35
Abdussamed DUZ <68667069+shaqablanca@users.noreply.github.com>
2023-07-13 00:50:28 +0200

    LinkedListSamples

---
+++ NextProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Algorithm
+{ 
+    class NextProject
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Next Algorithm Excersize will take place soon...");
+
+        }
+    } 
+}
---
+++ PowerOfNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace JumpGame
+{
+    class PowerOfNumbers
+    {
+
+        public int Power(int power, int number)
+        {
+            int result = 1;
+            if (power > 0)
+            {
+                for (int i = 0; i < power; i++)
+                {
+                    result *= number;
+                }
+            }
+
+            else if (power < 0)
+            {
+                number = 1 / number;
+                result *= number;
+            }
+            else
+            {
+                result = 1;
+            }
+            return result;
+
+        }
+
+        static void Main(String[] args)
+        {
+            PowerOfNumbers sonuc = new PowerOfNumbers();
+
+            Console.WriteLine(sonuc.Power(2, 15));
+        }
+    }
+
+}
